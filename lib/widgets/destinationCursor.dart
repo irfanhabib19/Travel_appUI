443563a656769075bbcafdf03e7fa97b72e5cd5d
habibirfan180 <habibirfan180@gmail.com>
2024-11-22 01:31:26 +0530

    destination place

--- lib/widgets/destinationCursor.dart
+++ lib/widgets/destinationCursor.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:travel_appui/models/destination_models.dart';
 
 class Destinationcursor extends StatelessWidget {
@@ -33,7 +34,6 @@ class Destinationcursor extends StatelessWidget {
         ),
         Container(
           height: 300,
-          color: const Color.fromARGB(255, 11, 135, 236),
           child: ListView.builder(
               scrollDirection: Axis.horizontal,
               itemCount: destinations.length,
@@ -43,35 +43,107 @@ class Destinationcursor extends StatelessWidget {
                     margin: EdgeInsets.all(10.0),
                     child: Container(
                       width: 210,
-                      color: Colors.red,
                       child: Stack(
+                        alignment: Alignment.topCenter,
                         children: [
+                          Positioned(
+                            bottom: 0,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.white,
+                              ),
+                              height: 120,
+                              width: 200,
+                              child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Align(
+                                      alignment: Alignment.bottomCenter,
+                                      child: Text(
+                                        '${desti.activities.length} activities',
+                                        style: TextStyle(
+                                            fontSize: 22,
+                                            fontWeight: FontWeight.w600,
+                                            color: Colors.black),
+                                      ),
+                                    ),
+                                    Align(
+                                      alignment: Alignment.bottomCenter,
+                                      child: Text(
+                                        '${desti.description}',
+                                        style:
+                                            TextStyle(color: Colors.blueGrey),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
                           Container(
                             decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: Colors.white,
-                            ),
-                            height: 120,
-                            width: 200,
-                            child: Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    '${desti.activities.length} activities',
-                                    style: TextStyle(
-                                        fontSize: 22,
-                                        fontWeight: FontWeight.w600,
-                                        color: Colors.black),
+                                borderRadius: BorderRadius.circular(20),
+                                color: Colors.white,
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: Colors.black,
+                                      offset: Offset(0.0, 2.0),
+                                      blurRadius: 6.0)
+                                ]),
+                            child: Stack(
+                              children: [
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(20),
+                                  child: Image(
+                                    height: 180,
+                                    width: 180,
+                                    image: AssetImage(desti.imageUrl),
+                                    fit: BoxFit.cover,
                                   ),
-                                  Text(
-                                    '${desti.description}',
-                                    style: TextStyle(color: Colors.blueGrey),
+                                ),
+                                Positioned(
+                                  left: 10.0,
+                                  bottom: 10.0,
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        desti.city,
+                                        style: TextStyle(
+                                            fontSize: 25,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white),
+                                      ),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        children: [
+                                          Icon(
+                                            FontAwesomeIcons.locationArrow,
+                                            size: 10.0,
+                                            color: Colors.white,
+                                          ),
+                                          SizedBox(
+                                            width: 5,
+                                          ),
+                                          Text(
+                                            desti.country,
+                                            style: TextStyle(
+                                                fontSize: 15,
+                                                fontWeight: FontWeight.w500,
+                                                color: Colors.white),
+                                          ),
+                                        ],
+                                      )
+                                    ],
                                   ),
-                                ],
-                              ),
+                                )
+                              ],
                             ),
                           )
                         ],
